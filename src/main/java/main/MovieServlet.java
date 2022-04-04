3e2994bf35b009a73c5a2f6e65c2bb2e253ec1cd
dbc-hou <david.collins.hou@gmail.com>
2022-04-04 15:30:36 -0500

    movies doGet and doPost

---
+++ src/main/java/main/MovieServlet.java
@@ -0,0 +1,60 @@
+package main;
+
+import com.google.gson.Gson;
+import data.Movie;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+@WebServlet(name = "MovieServlet", urlPatterns = "/movies/*")
+public class MovieServlet extends HttpServlet {
+    ArrayList<Movie> movies = new ArrayList<>();
+
+    int nextId = 1;
+
+    //  These methods are copied from CodeupClassroom.
+    //  They aren't very different from what I created, but they do at least work
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("application/json");
+
+        try {
+            PrintWriter out = response.getWriter();
+            String movieString = new Gson().toJson(movies.toArray());
+            out.println(movieString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+
+        BufferedReader br = request.getReader();
+
+        Movie[] newMovies = new Gson().fromJson(br, Movie[].class);
+        for (Movie movie : newMovies) {
+            movie.setId(nextId++);
+            movies.add(movie);
+        }
+
+        try {
+            PrintWriter out = response.getWriter();
+            out.println("Movie(s) added");
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
